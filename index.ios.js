c2ab133af7064c0d53c6337c2e11589704038dce
Charles Pernet <charlespernet@gmail.com>
2017-02-27 18:02:23 +0100

    working application tuto almost done

--- index.ios.js
+++ index.ios.js
@@ -8,34 +8,98 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
-import {fetchWeather} from './weatherApi'
+import {fetchWeather} from './weatherApi';
+import Highlighter from 'react-native-highlight-words';
+
+const iconNames = {
+  Clear: 'md-sunny',
+  Rain: 'md-rainy',
+  Thunderstorm: 'md-thunderstorm',
+  Clouds: 'md-cloud',
+  Snow: 'md-snow',
+  Drizzle: 'md-umbrella',
+  Fog: 'md-water'
+}
+
+const phrases = {
+  Clear: {
+    title: "It's Fucking Amaze Balls",
+    subtitle: "Rock that shit !",
+    highlight: "Fucking"
+  },
+  Rain: {
+    title: "Rain rain go away",
+    subtitle: "Stay inside and code all day",
+    highlight: "away"
+  },
+  Thunderstorm: {
+    title: "Fucking Thunderstrike",
+    subtitle: "unplug all devices",
+    highlight: "Fucking"
+  },
+  Clouds: {
+    title: "Cloud storage limit reached",
+    subtitle: "error 5000",
+    highlight: "limit"
+  },
+  Snow: {
+    title: "Brain Fucking Freeze",
+    subtitle: "Don't eat it !",
+    highlight: "Fucking"
+  },
+  Drizzle: {
+    title: "Meh... don't even ask",
+    subtitle: "What did I just say ?",
+    highlight: "don't"
+  },
+  Fog: {
+    title: "We can't see anything",
+    subtitle: "Where is my mind?",
+    highlight: "anything"
+  }
+}
 
 class App extends Component {
 
+  componentWillMount() {
+    this.state = {
+      temp:0,
+      weather:'Drizzle'
+    }
+  }
+
   componentDidMount() {
     this.getLocation()
-    fetchWeather(21, 22)
   }
 
   getLocation() {
     navigator.geolocation.getCurrentPosition(
-      (posData) => console.log(posData),
+      (posData) => fetchWeather(posData.coords.latitude, posData.coords.longitude)
+        .then(res => this.setState({
+          temp:Math.round(res.temp),
+          weather:res.weather
+        })),
       (error) => alert(error),
       {timeout: 10000}
     )
   }
+
 
   render() {
     return(
       <View style={styles.container}>
         <StatusBar hidden={true}/>
         <View style={styles.header}>
-          <Icon name={'ios-sunny'} size={80} color={'white'}/>
-          <Text style={styles.temp}>24 C</Text>
+          <Icon name={iconNames[this.state.weather]} size={80} color={'white'}/>
+          <Text style={styles.temp}>{this.state.temp} C</Text>
         </View>
         <View style={styles.body}>
-          <Text style={styles.title}>Awesome <Text style={{color: 'red'}}>first app</Text> using React Native</Text>
-          <Text style={styles.subtitle}>Make it cool</Text>
+          <Highlighter style={styles.title}
+            highlightStyle={{color: 'red'}}
+            searchWords={[phrases[this.state.weather].highlight]}
+            textToHighlight={phrases[this.state.weather].title}
+          />
+          <Text style={styles.subtitle}>{phrases[this.state.weather].subtitle}</Text>
         </View>
       </View>
     )
@@ -58,7 +122,7 @@ const styles = StyleSheet.create({
   temp: {
     fontFamily: 'HelveticaNeue-Bold',
     color: 'white',
-    fontSize: 24
+    fontSize: 32
   },
   body: {
     flex:5,
--- weatherApi.js
+++ weatherApi.js
@@ -1,6 +1,12 @@
-const baseUrl = 'api.openweathermap.org/data/2.5/weather?appId=98de7e17d772f2788de90725bbcd7f75';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=98de7e17d772f2788de90725bbcd7f75'
 
 export const fetchWeather = (lat, lon) => {
-  const url = baseUrl+"&lat="+lat+"&lon="+lon
-  console.log(url)
+  const url = baseUrl+"&lat="+lat+"&lon="+lon+'&units=metric'
+
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => ({
+      temp: json.main.temp,
+      weather: json.weather[0].main
+    }))
 }
